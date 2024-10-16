6d813eb0c5f058668b2474888cdbf2ea82165476
ndbaolam <baolam782004@gmail.com>
2024-10-17 01:55:48 +0700

    init

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const Sidebar = document.querySelector('#sidebar');
-console.log(Sidebar);
 const toggleSidebar = () => {
   Sidebar.classList.toggle('show');
 }
+
+const navList = document.querySelectorAll(".nav-list li, #sidebar li");
+navList.forEach((item, i) => {
+  if (item.hasAttribute('id')) {
+    item.addEventListener('click', () => {      
+      const id = item.getAttribute('id');      
+      const targetSections = document.querySelectorAll(`#${id}`);
+
+      const allSections = document.querySelectorAll('section, article, #sidebar ul');
+      allSections.forEach(section => {
+        section.style.display = 'none';
+      });
+
+      targetSections.forEach(item => {
+        item.style.display = 'block';
+      });
+    });
+  }
+});
